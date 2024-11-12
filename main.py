48f34f81f2e8cfa07aa137af9e3ec46c9787769b
Yoneibi Neira <yoneibi_10@gmail.com>
2024-11-12 18:22:52 -0500

    se actualiza codigo

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ from app.api.routes.endpoints import rutas
 
 
 from starlette.responses import RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
+
 
 #Crear las tablas de sql desde python
 Base.metadata.create_all(bind=engine)
 
 #Variable para administrar la aplicacion
 app=FastAPI()
+
+#Configurar el protocolo CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    
+
+
+)
+
 
 #Activar el api
 
